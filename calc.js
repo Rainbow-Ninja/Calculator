adad5e133ac298a5f788d7b47de189314806db54
Jo Dunham <scaffelpike@yahoo.com.au>
2020-08-23 00:29:14 +1000

    tests added

--- calc.js
+++ calc.js
@@ -1,1 +1,27 @@
 // For this to be complete I want this work to have SASS (yet to be added) and testing
+function add(a, b){
+    console.log("I'm in add")
+    return a + b;
+}
+
+function minus(a, b){
+    console.log("I'm in subtract")
+    return a - b;
+}
+
+function times(a, b){
+    console.log("I'm in multiply")
+    return a * b;
+}
+
+function divide(a, b){
+    console.log("I'm in division")
+    return a/b;
+}
+
+module.exports = {
+    add,
+    minus,
+    times,
+    divide
+}
